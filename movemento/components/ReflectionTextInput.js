7c745de0b7ad81828be4f6c4f748e36f78cddfd0
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-22 16:48:35 -0500

    gets to end of flow, time to make a request to the back end!

--- movemento/components/ReflectionTextInput.js
+++ movemento/components/ReflectionTextInput.js
@@ -2,10 +2,13 @@ import { TextInput } from 'react-native'
 import ContainerStyles from '../styles/Containers';
 import TextStyles from '../styles/Text';
 
-const ReflectionTextInput = () => {
+const ReflectionTextInput = ({ reflectionText, setReflectionText}) => {
+  
+
   return (
     <TextInput style={ContainerStyles.reflection}
       placeholder='Enter your reflection here'
+      onChangeText={setReflectionText}
     />
   )
 }
--- movemento/screens/ReflectionScreen.js
+++ movemento/screens/ReflectionScreen.js
@@ -2,12 +2,31 @@ import { View, Text, Pressable } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import ReflectionTextInput from '../components/ReflectionTextInput';
 import SaveEntryButton from '../components/buttons/SaveEntryButton';
+import { useJournalEntry } from '../context/JournalEntryContext';
+import { useState, useEffect } from 'react';
 
 const ReflectionScreen = () => {
   const navigation = useNavigation();
+  const { updateEntry } = useJournalEntry();
+
+  const [reflectionText, setReflectionText] = useState('')
+
+  useEffect(() => {
+      if (reflectionText) {
+        updateEntry({ reflection: reflectionText })
+      }
+    }, [reflectionText]);
+
+  const handleSave = () => {
+    //make the api post request
+  }
+  
   return(
     <View>
-      <ReflectionTextInput/>
+      <ReflectionTextInput
+        reflectionText={reflectionText}
+        setReflectionText={setReflectionText}
+      />
       <SaveEntryButton navigation={navigation}/>
     </View>
   )
