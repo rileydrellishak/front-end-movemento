45461c0e8cd238742e428fe1fd482d0c7f32379b
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-23 14:56:54 -0500

    fmbcl I do not like front end testing

---
+++ movemento/tests/MovementScreen.test.js
@@ -0,0 +1,8 @@
+import { render, fireEvent, rerender } from '@testing-library/react-native'
+import SelectMovementsScreen from '../screens/SelectMovementsScreen'
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+
+    // 1. Render the component
+    // 2. Find the element(s)
+    // 3. Assert expected behavior
+    // 4. Optional: simulate user interaction
--- movemento/tests/example.test.js
+++ movemento/tests/example.test.js
@@ -11,3 +11,27 @@ describe('Example', () => {
     })
   })
 })
+
+// Greeting.js
+import { Text } from 'react-native';
+import { render, getByText, rerender } from '@testing-library/react-native'
+
+const Greeting = ({ name }) => {
+  return <Text>Hello {name}</Text>;
+}
+
+describe('Greeting', () => {
+  it('renders the name', () => {
+    const { getByText, rerender } = render(<Greeting name='Riley'/>);
+
+    expect(getByText('Hello Riley')).toBeTruthy();
+
+    rerender(<Greeting name='Maille'/>);
+    expect(getByText('Hello Maille')).toBeTruthy()
+  }),
+  it('renders a component', () => {
+    const component = render(<Greeting name='Riley'/>);
+    expect(component).toHaveTextContent('Hello Riley')
+  })
+})
+
