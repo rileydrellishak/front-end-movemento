d05fb2e1e47ce005ec5f78bb13f003a43f1f0abf
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-30 21:18:04 -0500

    importing list from rn paper

--- movemento/screens/EditEntryScreen.js
+++ movemento/screens/EditEntryScreen.js
@@ -4,6 +4,7 @@ import ContainerStyles from '../styles/Containers'
 import { useData } from '../context/DataContext'
 import ReflectionTextInput from '../components/ReflectionTextInput'
 import ScreenStyles from '../styles/Screens'
+import { List } from 'react-native-paper'
 
 const EditEntryScreen = ({ route }) => {
   const { entry } = route.params
@@ -12,6 +13,7 @@ const EditEntryScreen = ({ route }) => {
   return (
     <SafeAreaView style={[ScreenStyles.editEntries]}>
       <Text>the id for the currently being edited entry is {entry.id} and it belongs to user with id of {entry.user_id}</Text>
+
       <ReflectionTextInput/>
       <Pressable style={[ButtonStyles.base, ButtonStyles.save]}>
         <Text>save changes</Text>
