ee114c50e8399259905daf7804ca7e2f90ed0112
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-30 21:38:16 -0500

    inserts reflection text input in accordion

--- movemento/screens/EditEntryScreen.js
+++ movemento/screens/EditEntryScreen.js
@@ -7,6 +7,8 @@ import ReflectionTextInput from '../components/ReflectionTextInput'
 import ScreenStyles from '../styles/Screens'
 import { List } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SelectableButtonsContainer from '../components/containers/SelectableButtonsContainer'
+import movements from '../data/movements'
 
 const EditEntryScreen = ({ route }) => {
   const { entry } = route.params
@@ -20,27 +22,31 @@ const EditEntryScreen = ({ route }) => {
   return (
     <SafeAreaView style={[ScreenStyles.editEntries]}>
       <Text>the id for the currently being edited entry is {entry.id} and it belongs to user with id of {entry.user_id}</Text>
-      <List.Section title='Accordions'>
+      <List.Section title='Accordions!'>
         <List.Accordion title='Select Movements'
           left={props => <List.Icon {...props} icon='dumbbell'/>}
           expanded={expanded}
           onPress={handlePress}
         >
-          <List.Item title='first'/>
+
+
         </List.Accordion>
         <List.Accordion title='Select Moods'
           left={props => <List.Icon {...props} icon='emoticon-happy-outline'/>}
           expanded={expanded}
           onPress={handlePress}
         >
-          <List.Item title='moods'/>
+          
+
+
         </List.Accordion>
         <List.Accordion title='Reflection'
           left={props => <List.Icon {...props} icon='thought-bubble'/>}
           expanded={expanded}
           onPress={handlePress}>
-            <List.Item title='type'/>
             <ReflectionTextInput reflectionText={entry.reflection}/>
+
+
         </List.Accordion>
       </List.Section>
       <Pressable style={[ButtonStyles.base, ButtonStyles.save]}>
