dea215fed1b4cb74fe31a03ba438f182315c3b24
Riley Drellishak <rileydrellishak@gmail.com>
2026-02-05 08:03:19 -0600

    hard coded the new debugging user as selected user (name is photographer)

--- movemento/context/JournalEntryContext.js
+++ movemento/context/JournalEntryContext.js
@@ -4,7 +4,7 @@ const JournalEntryContext = createContext();
 
 const getInitialJournalEntry = () => {
   return {
-    user_id: 1,
+    user_id: 9,
     movements: [],
     moods_before: [],
     moods_after: [],
