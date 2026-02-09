c012ebecee2228656771fba0861c47c13e072ef8
Riley Drellishak <rileydrellishak@gmail.com>
2026-02-09 12:59:01 -0600

    implements the calendar feature to highlight days that have entries

--- movemento/components/EntryCalendar.js
+++ movemento/components/EntryCalendar.js
@@ -2,21 +2,37 @@ import { CalendarProvider, Calendar } from 'react-native-calendars'
 import { useTheme } from 'react-native-paper'
 import { View } from 'react-native'
 const EntryCalendar = ({ entries }) => {
-  const theme = useTheme()
-  // const dateObj = new Date(entry.created_at)
-  // const displayDate = dateObj.toLocaleDateString() // mm/dd/year
-  // // markedDates must be in form year-mm-dd as string
-  // const formatDateToCustom = (date) => {
-  //   const year = date.getFullYear();
-  //   const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  //   const day = date.getDate().toString.padStart(2, '0')
-  //   return `${year}-${month}-${day}`
-  // }
-  // const markedDates = entries.map(formatDateToCustom)
+  const theme = useTheme();
+  const entryDates = entries.map((e) => {
+    return e.created_at
+  })
+
+  const formatDateToCustom = (dateString) => {
+    const dateObj = new Date(dateString)
+    return dateObj.toISOString().split('T')[0]
+  }
+
+  const markedDates = entryDates.map(formatDateToCustom)
+
+  const markedDatesObjects = markedDates.reduce(
+    (objects, date) => {
+      objects[date] = {
+        startingDay: true,
+        endingDay: true,
+        color: theme.colors.tertiary,
+        textColor: theme.colors.onTertiary
+      }
+      return objects
+    }, {}
+  )
 
   return (
-
-      <Calendar style={{backgroundColor: theme.colors.background}}date={new Date().toISOString()} onDayPress={(day) => console.log('Day pressed:', day)}/>
+      <Calendar
+        style={{backgroundColor: theme.colors.background}}
+        theme={{calendarBackground: theme.colors.background}}
+        date={new Date().toISOString()}
+        markingType={'period'}
+        markedDates={markedDatesObjects}/>
   )
 }
 
